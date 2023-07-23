aba897b7d28b4f4f7016b25adebe7b86c81920de
mdshihabuddin10 <mdshihab.uddin@bjitacademy.com>
2023-07-23 13:15:40 +0000

    changed

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -3,7 +3,7 @@ def call(Map config = [:]) {
                 withSonarQubeEnv(installationName: config.installationName) {
                     // Configure the SonarQube analysis properties
                     script {
-                        def scannerHome = tool '${config.tool}'
+                        def scannerHome = tool config.tool
                         sh "${scannerHome}/bin/sonar-scanner"
                     }
                 }
