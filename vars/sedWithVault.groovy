8bb0c8874264b7df93acd6aadbcbf1c4044b91f8
mdshihabuddin10 <mdshihab.uddin@bjitacademy.com>
2023-07-23 18:35:33 +0000

    chenged for now

--- vars/sedWithVault.groovy
+++ vars/sedWithVault.groovy
@@ -3,6 +3,7 @@ def call(Map config = [:]) {
             withVault(configuration: [timeout: 60, vaultCredentialId: config.vaultCredentialId, vaultUrl: config.vaultUrl], vaultSecrets: [[path: config.path, secretValues: [[isRequired: false, vaultKey: config.vaultKey]]]]) {
                         // some block
                         sh'echo value retrived'
+                        sh "sed -i 's/MYDBUSER/${config.vaultKey}/g' src/main/resources/application.properties"
                         
                 }
 
